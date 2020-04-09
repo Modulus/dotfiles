c590ac7c8ba6dd1ea7daf952af5707225e8ffd4a
Modulus <thejohnskauge@gmail.com>
2020-04-09 17:13:59 +0200

    Added windows stuff :O

---
+++ win/install.bash
@@ -0,0 +1,25 @@
+choco install 
+aws-iam-authenticator 0.5.0
+brave 1.5.122
+Cmder
+cpu-z 
+elm-platform 
+git
+glasswire
+golang 
+gpu-z 
+hwmonitor 
+hyper 
+jenkins-x 
+kubernetes-cli 
+kubernetes-helm 
+minikube
+nodejs-lts 
+openjdk 
+pgadmin4 
+python 
+python3 
+rust 
+slack 
+terraform 
+vagrant 
