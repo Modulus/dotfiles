47b292d44bf945642c157a352195bdd4f3907999
John Sigvald Skauge <johnsigvaldskauge@tv2.no>
2021-06-02 13:13:18 +0200

    Moved tfenv

--- mac/brew.sh
+++ mac/brew.sh
@@ -2,15 +2,15 @@
 echo "Installing brew packages"
 brew install kubernetes-cli python@3.8 helm	openssh	\
 minikube rbenv	sqlite	utf8proc autoconf gist moreutils p7zip readline	ssh-copy-id aws-iam-authenticator git helmfile vim \
-telnet watch coreutils highlight perl ruby hashicorp/tap/terraform gnu-getopt	tldr wget \
-eksctl	ipcalc node findutils tree go kind lua sops rustup-init java11 pyenv  libpq docker-compose jq haskell-stack
+telnet watch coreutils highlight perl ruby gnu-getopt	tldr wget \
+eksctl	ipcalc node findutils tree go kind lua sops rustup-init java11 pyenv  libpq docker-compose jq haskell-stack \
+tfenv
 
 
 echo "Installing brew cask packages"
 brew cask install  discord firefox  intellij-idea-ce spectacle  vlc \
 docker flux google-cloud-sdk iterm2 signal spotify  \
 dropbox google-backup-and-sync google-hangouts jetbrains-toolbox postman slack vagrant visual-studio-code google-cloud-sdk firefox brave-browser  visual-studio-code  \
-tfenv
 
 
 # virtualbox virtualbox-extension-pack
