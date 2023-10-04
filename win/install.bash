a777177ab8141298aeacecbab4c83fd200b2b23f
Modulus <thejohnskauge@gmail.com>
2023-10-04 22:37:30 +0200

    Added handbrake to install

--- win/install.bash
+++ win/install.bash
@@ -1,12 +1,16 @@
-choco install -y Cmder git glasswire golang hwmonitor kubernetes-cli kubernetes-helm minikube nodejs-lts openjdk pgadmin4 python3 slack terraform vagrant intellijidea-community qbittorrent spotify etcher discord docker-desktop visualstudio-2019-remotetools postman realvnc rufus kind kdenlive krita mingw multipass notion vlc vscode hwmonitor gpu-z cpu-z crystaldiskinfo azure-cli gimp putty filezilla foxitreader
 
-notion
+# minikube 
+choco install -y Cmder git glasswire golang hwmonitor kubernetes-cli kubernetes-helm nodejs-lts openjdk pgadmin4 python3 slack terraform vagrant intellijidea-community qbittorrent spotify etcher discord docker-desktop visualstudio-2019-remotetools postman realvnc rufus kind kdenlive krita mingw multipass notion vlc vscode hwmonitor gpu-z cpu-z crystaldiskinfo azure-cli gimp putty filezilla foxitreader
+
+
+choco install -y signal windirstat evernote 7zip wireshark handbrake
+
+crystaldiskinfo
 
 #aws-iam-authenticator 
 
 # ninite and manual
-signal 
-windirstat 
 crystaldiskinfo
-notion
-evernote
+
+https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170
+
