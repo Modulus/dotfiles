1c54bc2c2a4ee3be638e878058a82002a65871ac
Modulus <thejohnskauge@gmail.com>
2023-12-29 15:56:21 +0100

    Added missing libs for virt-manager

---
+++ linux/virt-manager.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt install qemu-kvm virt-manager virtinst libvirt-clients bridge-utils libvirt-daemon-system -y
+sudo apt install libspice-client-gtk-3.0-5 -y
+sudo apt install qemu-utils -y 
