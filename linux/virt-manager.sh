70e67ac9ab9a8124ad1ae920e8cc84cef5487206
Modulus <thejohnskauge@gmail.com>
2024-01-16 14:51:32 +0100

    Added missing config

--- linux/virt-manager.sh
+++ linux/virt-manager.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt install qemu-kvm virt-manager virtinst libvirt-clients bridge-utils libvirt-daemon-system -y
-sudo apt install libspice-client-gtk-3.0-5 -y
+#sudo apt install libspice-client-gtk-3.0-5 -y
 sudo apt install qemu-utils -y 
+sudo apt install -y gir1.2-spiceclientgtk-3.0
