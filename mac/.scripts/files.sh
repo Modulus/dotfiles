f141ae20bba74338aa3145a443d04b60b90c939c
John Sigvald Skauge <john.sigvald.skauge@dnb.no>
2024-10-02 12:08:58 +0200

    BUmped limits

--- mac/.scripts/files.sh
+++ mac/.scripts/files.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 #http://blog.mact.me/2014/10/22/yosemite-upgrade-changes-open-file-limit
-echo kern.maxfiles=65536 | sudo tee -a /etc/sysctl.conf
-echo kern.maxfilesperproc=65536 | sudo tee -a /etc/sysctl.conf
-sudo sysctl -w kern.maxfiles=65536
-sudo sysctl -w kern.maxfilesperproc=65536
-ulimit -n 65536 65536
+#echo kern.maxfiles=65536 | sudo tee -a /etc/sysctl.conf
+#echo kern.maxfilesperproc=65536 | sudo tee -a /etc/sysctl.conf
+#sudo sysctl -w kern.maxfiles=65536
+#sudo sysctl -w kern.maxfilesperproc=65536
+#ulimit -n 65536 65536
+
+
+
+echo kern.maxfiles=524288 | sudo tee -a /etc/sysctl.conf
+echo kern.maxfilesperproc=524288 | sudo tee -a /etc/sysctl.conf
+sudo sysctl -w kern.maxfiles=524288
+sudo sysctl -w kern.maxfilesperproc=524288
+ulimit -n 524288
