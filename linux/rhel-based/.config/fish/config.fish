cb15cd297cd2c532c294c8d8301a0909134908ff
John Sigvald Skauge <496971+Modulus@users.noreply.github.com>
2025-01-29 20:14:03 +0100

    Added config for fish shell

--- linux/rhel-based/.config/fish/config.fish
+++ linux/rhel-based/.config/fish/config.fish
@@ -1,33 +1,20 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+# Ollama stash
+alias o=ollama
+
+
+# Golang stash
+export GOPATH=$HOME/go
+
+
+
 starship init fish | source
 
 
-#PYthon stash
-#export PYENV_ROOT="$HOME/.pyenv"
-#[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-#eval "$(pyenv init -)"
-#eval "$(pyenv virtualenv-init -)"
-
-
-# Set kubeconfig and k8s stuff
-export KUBECONFIG="$HOME/.kube/microk8s:$HOME/.kube/config"
-
-export PATH="$PATH:/var/lib/snapd/snap/bin"
-
-
-export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin:$HOME/.local/bin"
-export RUST_LOG=info
-
-
-#source <(kubectl completion zsh)  # set up autocomplete in zsh into the current shell
-#echo '[[ $commands[kubectl] ]] && source <(kubectl completion zsh)' >> ~/.zshrc # add autocomplete permanently to your zsh shell
-#[[ $commands[kubectl] ]] && source <(kubectl completion zsh)
-
 alias k=kubectl
-#complete -o default -F __start_kubectl k
-source ~/.env
 
-. "$HOME/.cargo/env"
 
+# pyenv
+ pyenv init - fish | source
