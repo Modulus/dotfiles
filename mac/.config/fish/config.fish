0e0158379d387ce489233a28357619812081108e
John Sigvald Skauge <john.sigvald.skauge@dnb.no>
2025-01-30 10:33:39 +0100

    nvm up and running

--- mac/.config/fish/config.fish
+++ mac/.config/fish/config.fish
@@ -11,7 +11,12 @@ starship init fish | source
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+
+# Kubernetes
+
 alias k=oc
+fish_add_path -g ~/.krew/bin
+
 
 
 # pyenv
@@ -34,4 +39,13 @@ export GOPATH=$HOME/go
 
 # Rust
 fish_add_path -g ~/.cargo/bin
+
+alias bump=helm-chart-version-bumper
+
+# Node
+fish_add_path -g ~/.nvm/versions/node/v18.20.4/bin
+
+ export NVM_DIR="$HOME/.nvm"
+ # [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+ #[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
